dd170602f745ac72444e5df854c617e173cd885f
hstrecker <81708770+hstrecker@users.noreply.github.com>
2023-04-11 21:13:22 -0700

    Update Dash.py

--- Dash.py
+++ Dash.py
@@ -8,9 +8,6 @@ import plotly.graph_objects as go
 # Code to make compatible with streamlit
 import signal
 import sys
-def default_handler(signum, frame):
-    print(f"Received signal {signum}. Exiting.")
-    sys.exit(0)
 
 # Import Existing Data
 generation_df = pd.read_csv('ModuleData/electricity_generation.csv')
@@ -410,5 +407,8 @@ def update_fig4(tab_4_checklist):
 
 
 if __name__ == '__main__':
-    
+    app.run_server(debug=True)
+    def default_handler(signum, frame):
+    print(f"Received signal {signum}. Exiting.")
+    sys.exit(0)
     signal.signal(signal.SIGTERM, default_handler)
